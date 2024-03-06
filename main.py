233419b79d81fb5e0493f66b664534cee32ca317
up202108677 <up202108677@fe.up.pt>
2024-03-06 16:22:26 +0000

    nEw MeNu

--- main.py
+++ main.py
@@ -1,26 +1,68 @@
-filePath = "inputs/b_little_bit_of_everything.in"
+import random
 
-with open(filePath, "r") as file:
-    lines = file.readlines()
+def menu():
+    choice = ""
+    while choice != "f":
+        print("\nChoose a file to read:")
+        print("a. a_example")
+        print("b. b_little_bit_of_everything")
+        print("c. c_many_ingredients")
+        print("d. d_many_pizzas")
+        print("e. e_many_teams")
+        print("f. Exit")
+        choice = input("\nEnter your choice: ")
 
-initialInfo = lines[0].split()
+        if choice == "f":
+            print("Exiting program...")
+            break
 
-teams = {}
+        file_mapping = {
+            "a": "inputs/a_example.in",
+            "b": "inputs/b_little_bit_of_everything.in",
+            "c": "inputs/c_many_ingredients.in",
+            "d": "inputs/d_many_pizzas.in",
+            "e": "inputs/e_many_teams.in"
+        }
 
-print(initialInfo[0] + " pizzas")
-for team in range(1, len(initialInfo)):
-    teams.update({team + 1: initialInfo[team]})
+        filePath = file_mapping.get(choice)
+        if filePath is None:
+            print("\nInvalid choice!")
+            continue
 
-pizzas = {}
+        with open(filePath, "r") as file:
+            lines = file.readlines()
 
-i = 0
+        initialInfo = lines[0].split()
 
-for line in lines[1:]:
-    items = line.split()
-    pizzas.update({(i, items[0]): items[1:]})
-    i += 1
+        teams = {}
+        print(initialInfo[0] + " pizzas")
+        for team in range(1, len(initialInfo)):
+            teams.update({team + 1: initialInfo[team]})
+
+        pizzas = {}
+        i = 0
+        for line in lines[1:]:
+            items = line.split()
+            pizzas.update({(i, items[0]): items[1:]})
+            i += 1
+
+        print(teams)
+        print("")
+        print(pizzas)
+
+        best_solution = initial_solution(pizzas, teams)
+        print("\nBest Solution:")
+        print(best_solution)
+        
+        input("\nPress Enter to continue...")
 
 
-print(teams)
-print("")
-print(pizzas)
+
+def initial_solution(pizzas, teams):
+    solution = {}
+    for (team_id, num_pizzas) in teams.items():
+        solution[team_id] = random.sample(list(pizzas.keys()), min(int(num_pizzas), len(pizzas)))
+    return solution
+
+
+menu()
