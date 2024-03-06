63d8f95ce1af89df954a03751975f5641d70741f
up202108677 <up202108677@fe.up.pt>
2024-03-06 11:32:19 +0000

    rEaDiNgZ fIlEz AnD dAtA sTrUcTuReS

--- main.py
+++ main.py
@@ -1,14 +1,26 @@
-filePath = "input.txt"
+filePath = "b_little_bit_of_everything.in"
 
 with open(filePath, "r") as file:
     lines = file.readlines()
 
 initialInfo = lines[0].split()
 
+teams = {}
+
 print(initialInfo[0] + " pizzas")
 for team in range(1, len(initialInfo)):
-    print(initialInfo[team] + " team of " + str(team + 1))
+    teams.update({team + 1: initialInfo[team]})
 
-print("")
+pizzas = {}
+
+i = 0
+
 for line in lines[1:]:
-    print(line.strip())
+    items = line.split()
+    pizzas.update({(i, items[0]): items[1:]})
+    i += 1
+
+
+print(teams)
+print("")
+print(pizzas)
