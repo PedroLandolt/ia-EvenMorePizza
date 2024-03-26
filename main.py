8f03975e50ff5f668b0f30bcc65de0475cb55951
JCoelho13 <jantonioboavista@gmail.com>
2024-03-26 18:29:59 +0000

    chouriças chouriçadas

--- main.py
+++ main.py
@@ -97,6 +97,30 @@ def menu():
         scores = evaluateSolution(best_solution, pizzas)
         print(scores)
         print("Total score: " + str(sum(scores)))
+
+        # Tabu Search Solution
+        print("\nTabu Search Solution:")
+        tabuListSize = 10
+        maxIterations = 1000
+        best_solution = tabuSearch(pizzas, teams, tabuListSize, maxIterations)
+        print(best_solution)
+        print("Scores from each team:")
+        scores = evaluateSolution(best_solution, pizzas)
+        print(scores)
+        print("Total score: " + str(sum(scores)))
+
+        # Genetic Algorithm Solution
+        print("\nGenetic Algorithm Solution:")
+        population_size = 100
+        tournament_size = 10
+        mutation_rate = 0.1
+        max_generations = 100
+        best_solution = genetic_algorithm(pizzas, teams, population_size, tournament_size, mutation_rate, max_generations)
+        print(best_solution)
+        print("Scores from each team:")
+        scores = evaluateSolution(best_solution, pizzas)
+        print(scores)
+        print("Total score: " + str(sum(scores)))
         
 
         input("\nPress Enter to continue...")
@@ -211,5 +235,97 @@ def simulatedAnnealing(pizzas, teams, initialTemperature, finalTemperature, cool
         temperature *= 1 - coolingRate
 
     return bestSolution
+
+# tabu search
+
+def tabuSearch(pizzas, teams, tabuListSize, maxIterations):
+    currentSolution = randomSolution(pizzas, teams)
+    currentScore = sum(evaluateSolution(currentSolution, pizzas))
+    bestSolution = currentSolution
+    bestScore = currentScore
+    tabuList = []
+    tabuList.append(currentSolution)
+    tabuListSize = tabuListSize
+    maxIterations = maxIterations
+    i = 0
+
+    while i < maxIterations:
+        i += 1
+        newSolution = getNeighbourSolution(currentSolution)
+        newScore = sum(evaluateSolution(newSolution, pizzas))
+
+        if newSolution not in tabuList:
+            if newScore > bestScore:
+                bestSolution = newSolution
+                bestScore = newScore
+            currentSolution = newSolution
+            currentScore = newScore
+            tabuList.append(newSolution)
+            if len(tabuList) > tabuListSize:
+                tabuList.pop(0)
+
+    return bestSolution
+
+def initialize_population(pizzas, teams, population_size):
+    population = []
+    for _ in range(population_size):
+        population.append(randomSolution(pizzas, teams))
+    return population
+
+def tournament_selection(population, tournament_size, pizzas):
+    selected_parents = []
+    for _ in range(2):  # Select 2 parents
+        tournament_candidates = random.sample(population, tournament_size)
+        selected_parents.append(max(tournament_candidates, key=lambda x: sum(evaluateSolution(x, pizzas))))
+    return selected_parents
+
+def single_point_crossover(parents):
+    min_length = min(len(parents[0]), len(parents[1]))
+    
+    if min_length <= 1:
+        # If either parent has length 1 or less, cannot perform crossover
+        return parents
+
+    crossover_point = random.randint(1, min_length - 1)
+    child1 = parents[0][:crossover_point] + parents[1][crossover_point:]
+    child2 = parents[1][:crossover_point] + parents[0][crossover_point:]
+    return child1, child2
+
+
+def mutation(solution, mutation_rate, pizzas):
+    mutated_solution = solution.copy()
+    for i, team in enumerate(mutated_solution):
+        if random.random() < mutation_rate:
+            random_pizza_index = random.randint(0, len(team[1]) - 1)
+            mutated_solution[i][1][random_pizza_index] = random.choice(list(pizzas.keys()))
+    return mutated_solution
+
+def genetic_algorithm(pizzas, teams, population_size, tournament_size, mutation_rate, max_generations):
+    population = initialize_population(pizzas, teams, population_size)
+    best_solution = None
+    best_score = float('-inf')
+    generation = 0
+
+    while generation < max_generations:
+        new_population = []
+
+        for _ in range(population_size // 2):
+            parents = tournament_selection(population, tournament_size, pizzas)
+            offspring1, offspring2 = single_point_crossover(parents)
+            offspring1 = mutation(offspring1, mutation_rate, pizzas)
+            offspring2 = mutation(offspring2, mutation_rate, pizzas)
+            new_population.extend([offspring1, offspring2])
+
+        population = new_population
+        generation += 1
+
+        # Evaluate population and update best solution
+        for solution in population:
+            score = sum(evaluateSolution(solution, pizzas))
+            if score > best_score:
+                best_solution = solution
+                best_score = score
+
+    return best_solution
 
 menu()
